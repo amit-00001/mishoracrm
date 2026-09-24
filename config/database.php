83de2ccff2a80a14869d6f1db9690b4a488d614f
Amit <152647354+amit-00001@users.noreply.github.com>
2026-09-24 11:48:00 +0530

    Update SSL CA path for MySQL connection

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => storage_path('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => storage_path('certs/ca.pem'),
             ]) : [],
         ],
 
